f7bbbd748c82f059576f1d8d373e0fd9b6112455
David Ryan <goliathstone4@gmail.com>
2024-03-30 23:21:00 -0500

    eod

--- addons/debug_menu/debug_menu.gd
+++ addons/debug_menu/debug_menu.gd
@@ -82,6 +82,13 @@ func _init() -> void:
 	# This must be done here instead of `_ready()` to avoid having `visibility_changed` be emitted immediately.
 	visible = false
 
+	if not InputMap.has_action("cycle_debug_menu"):
+		# Create default input action if no user-defined override exists.
+		# We can't do it in the editor plugin's activation code as it doesn't seem to work there.
+		InputMap.add_action("cycle_debug_menu")
+		var event := InputEventKey.new()
+		event.keycode = KEY_F3
+		InputMap.action_add_event("cycle_debug_menu", event)
 
 
 func _ready() -> void:
